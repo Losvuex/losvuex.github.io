1a1fa497b59c8dbdb4f6b804249299ee94cb6455
DMCKG1999 <dmckg1999@gmail.com>
2023-10-14 15:02:24 +0300

    update index

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const fileName = document.querySelector(".fileName");
 
 const fileInput = document.querySelector("input[type=file]");
-fileInput.addEventListener("change", function() {
-    if (this.files && this.files[0]) {
-        fileName.innerHTML = this.files[0].name;
+fileInput.addEventListener("change", (e) => {
+    const fd = new FormData();
+
+  // add all selected files
+  e.target.files.forEach((file) => {
+    fd.append(e.target.name, file, file.name);  
+  });
+  
+  // create the request
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // we done!
     }
+  };
+  
+  // path to server would be where you'd normally post the form to
+  xhr.open('POST', '/path/to/server', true);
+  xhr.send(fd);
 });
 
