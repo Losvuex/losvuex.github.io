09652f3dc722974dc5643fa007c078acd435791a
DMCKG1999 <dmckg1999@gmail.com>
2023-10-14 15:12:57 +0300

    update script

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-const fileName = document.querySelector(".fileName");
+const socket = new WebSocket("ws://51.250.121.92:8080");
 
 const fileInput = document.querySelector("input[type=file]");
 fileInput.addEventListener("change", (e) => {
     const fd = new FormData();
 
-  // add all selected files
   e.target.files.forEach((file) => {
     fd.append(e.target.name, file, file.name);  
   });
   
-  // create the request
-  const xhr = new XMLHttpRequest();
-  xhr.onload = () => {
-    if (xhr.status >= 200 && xhr.status < 300) {
-        // we done!
-    }
-  };
-  
-  // path to server would be where you'd normally post the form to
-  xhr.open('POST', '/path/to/server', true);
-  xhr.send(fd);
+  socket.send(fd);
 });
 
